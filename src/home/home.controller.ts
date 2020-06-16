06d3a4c5990087dfdd4b88601f5115feb4225700
Ming <huangjunrui>
2020-06-16 23:50:42 +0800

    add

--- src/home/home.controller.ts
+++ src/home/home.controller.ts
@@ -29,4 +29,25 @@ export class HomeController {
     };
 
 
+    /**
+     * 获取首页轮播
+     */
+    @Get('/getSwiper')
+    getSwiper() {
+        return [
+            {
+                "name": '测试1',
+                "path": 'https://img20.360buyimg.com/babel/s590x470_jfs/t1/143671/13/779/56208/5ee77a4eE2c43007a/e208f572863d3384.jpg'
+            },
+            {
+                "name": '测试2',
+                "path": 'https://img14.360buyimg.com/pop/s590x470_jfs/t1/118475/13/9913/31481/5ee1e6bcE3f86cb18/3e771d066e7f349b.jpg'
+            },
+            {
+                "name": '测试3',
+                "path": 'https://img14.360buyimg.com/pop/s590x470_jfs/t1/129988/5/568/60031/5ece7096Efecabaef/3c234c60cf2bdf21.jpg'
+            },
+        ];
+    };
+
 }
